c11f1ff097b9c5f29d0479536d38dc6f3d9613b9
Melquisedeque Pereira <melqui1@live.com.pt>
2019-08-27 16:34:46 +0100

    Added jQuery to the project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-(function() {
+(function ($) {
 
 	const log = () => console.log.apply(console, arguments);
 	log('main.js is working');
-	
+
 	// menu animation
-	document.querySelector('.hamburger').addEventListener('click', function() {
-		!this.classList.contains('is-active') ? this.classList.add('is-active') : this.classList.remove('is-active');
+	$('.hamburger').on('click', function () {
+		$(this).toggleClass('is-active');
 	});
 
-})();
+	// Droom parallax effect
+	
+
+	// smooth scrolling
+	$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top - 70
+				}, 1000, function () {
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) {
+						return false;
+					} else {
+						$target.attr('tabindex', '-1');
+						$target.focus();
+					}
+				});
+			}
+		}
+	});
+})(jQuery);
